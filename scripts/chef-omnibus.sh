51de36d04c60d28c077ec028ac836f574d673ea3
Timothy Sutton <tim@synthist.net>
2013-09-10 09:20:02 -0400

    Changes for 10.9 support
    - Detect a 10.9 ESD and make the new ISO out of BaseSystem.dmg by converting to RW, growing it and installing Extras there
      - It's very possible there is a more efficient method to do this
    - Xcode CLI tools script uses install-on-demand trigger file and a bunch of shell pipes to install from SUS

--- scripts/chef-omnibus.sh
+++ scripts/chef-omnibus.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# http://wiki.opscode.com/display/chef/Installing+Omnibus+Chef+Client+on+Linux+and+Mac
+# http://www.opscode.com/chef/install
 curl -L https://www.opscode.com/chef/install.sh | bash
 
-exit
+# Force exiting zero because the installer doesn't yet recognize 10.9 as
+# a valid install target
+exit 0
