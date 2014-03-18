b9c07664582a3b756af993cef695af9fedc9d6c1
Timothy Sutton <tim@synthist.net>
2014-03-18 17:04:56 -0400

    Puppet/Facter install now installs any version thanks to AutoPkg recipes

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -1,18 +1,60 @@
 #!/bin/sh
 
-# This uses Hashicorp's puppet-bootstrap script for OS X. We override
-# the URLs because they're probably more recent than those in the script.
+# Install the latest Puppet and Facter using AutoPkg recipes
+# https://github.com/autopkg/autopkg
+#
+# PUPPET_VERSION and FACTER_VERSION can be overridden with specific versions,
+# or "latest" to get the latest stable versions
 
-PUPPET=http://downloads.puppetlabs.com/mac/puppet-3.2.3.dmg
-FACTER=http://downloads.puppetlabs.com/mac/facter-1.7.2.dmg
+PUPPET_VERSION=${PUPPET_VERSION:-latest}
+FACTER_VERSION=${FACTER_VERSION:-latest}
 
-curl -Ok https://raw.github.com/hashicorp/puppet-bootstrap/master/mac_os_x.sh
-chmod +x mac_os_x.sh
+# install function mostly borrowed dmg function from hashicorp/puppet-bootstrap,
+# except we just take an already-downloaded dmg
+function install_dmg() {
+    local name="$1"
+    local dmg_path="$2"
 
-FACTER_PACKAGE_URL=$FACTER \
-PUPPET_PACKAGE_URL=$PUPPET \
-./mac_os_x.sh
+    echo "Installing: ${name}"
 
-rm mac_os_x.sh
+    # Mount the DMG
+    echo "-- Mounting DMG..."
+    tmpmount=$(/usr/bin/mktemp -d /tmp/puppet-dmg.XXXX)
+    hdiutil attach "${dmg_path}" -mountpoint "${tmpmount}"
 
-exit
+    echo "-- Installing pkg..."
+    pkg_path=$(find "${tmpmount}" -name '*.pkg' -mindepth 1 -maxdepth 1)
+    installer -pkg "${pkg_path}" -tgt /
+
+    # Unmount
+    echo "-- Unmounting and ejecting DMG..."
+    hdiutil eject "${tmpmount}"
+}
+
+function get_dmg() {
+    local recipe_name="$1"
+    local version="$2"
+
+    # Run AutoPkg setting VERSION, and saving the results as a plist
+    "${AUTOPKG}" run --report-plist ${recipe_name} -k VERSION="${version}" > /tmp/autopkg-puppet-report.plist
+    echo $(/usr/libexec/PlistBuddy -c 'Print :new_downloads:0' /tmp/autopkg-puppet-report.plist)
+}
+
+# Get AutoPkg
+AUTOPKG_DIR=$(mktemp -d /tmp/autopkg-XXXX)
+git clone https://github.com/autopkg/autopkg "$AUTOPKG_DIR"
+AUTOPKG="$AUTOPKG_DIR/Code/autopkg"
+
+# Add the recipes repo containing Puppet/Facter
+"${AUTOPKG}" repo-add recipes
+
+# Retrieve the installer DMGs
+PUPPET_DMG=$(get_dmg Puppet.download "${PUPPET_VERSION}")
+FACTER_DMG=$(get_dmg Facter.download "${FACTER_VERSION}")
+
+# Install them
+install_dmg "Puppet" "${PUPPET_DMG}"
+install_dmg "Facter" "${FACTER_DMG}"
+
+# Clean up
+rm -rf "${PUPPET_DMG}" "${FACTER_DMG}" "~/Library/AutoPkg"
