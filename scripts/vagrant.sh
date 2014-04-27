c7a9498a56731028772152de684feb79e7806adf
Timothy Sutton <tim@synthist.net>
2014-04-27 07:25:45 -0400

    Vagrant key - don't use -k

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -10,6 +10,6 @@ scutil --set HostName ${COMPNAME}.vagrantup.com
 # Installing vagrant keys
 mkdir /Users/vagrant/.ssh
 chmod 700 /Users/vagrant/.ssh
-curl -k -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > /Users/vagrant/.ssh/authorized_keys
+curl -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > /Users/vagrant/.ssh/authorized_keys
 chmod 600 /Users/vagrant/.ssh/authorized_keys
 chown -R vagrant /Users/vagrant/.ssh
