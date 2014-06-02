b09116ee4ae905ff494c6b6f6bd7d08d8dfc80a5
Timothy Sutton <tim@synthist.net>
2014-06-02 13:49:20 -0400

    Chef supports 10.9, no exit 0 needed anymore

--- scripts/chef-omnibus.sh
+++ scripts/chef-omnibus.sh
@@ -2,7 +2,3 @@
 
 # http://www.opscode.com/chef/install
 curl -L https://www.opscode.com/chef/install.sh | bash
-
-# Force exiting zero because the installer doesn't yet recognize 10.9 as
-# a valid install target
-exit 0
