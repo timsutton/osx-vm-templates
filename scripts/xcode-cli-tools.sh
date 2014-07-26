a7d5a399d6a6c20145bf8391812ee12df7e3e9c4
Radek Simko <radek.simko@gmail.com>
2014-07-26 22:34:42 +0200

    Support for OS X 10.10 added

--- scripts/xcode-cli-tools.sh
+++ scripts/xcode-cli-tools.sh
@@ -3,19 +3,20 @@
 # Get and install Xcode CLI tools
 OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
  
-# on 10.9, we can leverage SUS to get the latest CLI tools
+# on 10.9+, we can leverage SUS to get the latest CLI tools
 if [ "$OSX_VERS" -ge 9 ]; then
 
     # create the placeholder file that's checked by CLI updates' .dist code 
     # in Apple's SUS catalog
     touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
-    # find the update with "Developer" in the name
-    PROD=$(softwareupdate -l | grep -B 1 "Developer" | head -n 1 | awk -F"*" '{print $2}')
+    # find the CLI Tools update
+    [ "$OSX_VERS" -ge 10 ] && PROD=$(softwareupdate -l | grep "Command Line" | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
+    [ "$OSX_VERS" -eq 9 ] && PROD=$(softwareupdate -l | grep -B 1 "Developer" | head -n 1 | awk -F"*" '{print $2}')
 
     # install it
     # amazingly, it won't find the update if we put the update ID in double-quotes
-    softwareupdate -i $PROD -v
+    softwareupdate -i "$PROD" -v
  
 # on 10.7/10.8, we instead download from public download URLs, which can be found in
 # the dvtdownloadableindex:
