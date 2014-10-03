b001475df54a9808d3d56d06e71b8fa3001fff42
Timothy Sutton <tim@synthist.net>
2014-10-03 17:12:15 -0400

    Cleanup script whitespace

--- scripts/xcode-cli-tools.sh
+++ scripts/xcode-cli-tools.sh
@@ -5,13 +5,10 @@ OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
  
 # on 10.9+, we can leverage SUS to get the latest CLI tools
 if [ "$OSX_VERS" -ge 9 ]; then
-
     # create the placeholder file that's checked by CLI updates' .dist code 
     # in Apple's SUS catalog
     touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
-
     # find the CLI Tools update
-
     PROD=$(softwareupdate -l | grep "Command Line" | head -n 1 | awk -F"*" '{print $2}' | sed -e 's/^ *//' | tr -d '\n')
     # install it
     softwareupdate -i "$PROD" -v
