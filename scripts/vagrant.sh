d0b6c477159b47fe877915694393f7b982f00e2d
Tom Taylor <tom@tomtaylor.co.uk>
2014-11-04 10:39:58 +0000

    Extract vagrant settings into user variables.
    The defaults still build a vagrant compatible image as before, but now there
    are variables to configure the username, password, whether the user is
    automatically logged in, and whether the vagrant SSH keys are installed.

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
-date > /etc/vagrant_box_build_time
+date > /etc/box_build_time
 OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
 
 # Set computer/hostname
-COMPNAME=vagrant-osx-10-${OSX_VERS}
+COMPNAME=osx-10_${OSX_VERS}
 scutil --set ComputerName ${COMPNAME}
 scutil --set HostName ${COMPNAME}.vagrantup.com
 
-# Installing vagrant keys
-mkdir /Users/vagrant/.ssh
-chmod 700 /Users/vagrant/.ssh
-curl -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > /Users/vagrant/.ssh/authorized_keys
-chmod 600 /Users/vagrant/.ssh/authorized_keys
-chown -R vagrant /Users/vagrant/.ssh
+# Packer passes boolean user variables through as '1', but this might change in
+# the future, so also check for 'true'.
+if [ "$ROOT_VAGRANT_KEYS" = "true" ] || [ "$ROOT_VAGRANT_KEYS" = "1" ]; then
+	echo "Installing vagrant keys for $ROOT_USERNAME user"
+	mkdir "/Users/$ROOT_USERNAME/.ssh"
+	chmod 700 "/Users/$ROOT_USERNAME/.ssh"
+	curl -L 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' > "/Users/$ROOT_USERNAME/.ssh/authorized_keys"
+	chmod 600 "/Users/$ROOT_USERNAME/.ssh/authorized_keys"
+	chown -R "$ROOT_USERNAME" "/Users/$ROOT_USERNAME/.ssh"
+fi
 
-# Create vagrant group and assign vagrant user to it
-dseditgroup -o create vagrant
-dseditgroup -o edit -a vagrant vagrant
+# Create a group and assign the user to it
+dseditgroup -o create "$ROOT_USERNAME"
+dseditgroup -o edit -a "$ROOT_USERNAME" "$ROOT_USERNAME"
