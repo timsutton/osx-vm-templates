d00d75e3f90a5f509710479bce2f99791800ecf5
Jon Ander Hernandez <jonan.h@gmail.com>
2017-09-07 22:18:52 +0300

    High Sierra fixes: Check new location of BaseSystem.dmg, use APFS and use InstallInfo.plist instead OSInstall.mpkg package

--- prepare_iso/prepare_vdi.sh
+++ prepare_iso/prepare_vdi.sh
@@ -23,12 +23,9 @@
 #
 #
 
-. $(dirname "$0")/create_firstboot_pkg.sh
-
 usage() {
 	cat <<EOF
 Usage:
-$(basename "$0") [-upiD] "/path/to/InstallESD.dmg" /path/to/output/directory
 $(basename "$0") [-upiD] "/path/to/Install OS X [Name].app" /path/to/output/directory
 
 Description:
@@ -58,17 +55,20 @@ EOF
 }
 
 cleanup() {
-	hdiutil detach -quiet -force "$MNT_ESD" || echo > /dev/null
-	hdiutil detach -quiet -force "$MNT_BASE_SYSTEM" || echo > /dev/null
-
-	if [ ! -z "$TEST" ]; then
-		cp "$BASE_SYSTEM_DMG_RW_SPARSE" "$BASE_SYSTEM_DMG_RW_SPARSE.back"
+	if [ ! -z "$MNT_ESD" ]; then
+		hdiutil detach -quiet -force "$MNT_ESD" || rm -rf "$MNT_ESD" || echo > /dev/null
 	fi
-	rm -rf "$MNT_ESD" "$MNT_BASE_SYSTEM" "$BASE_SYSTEM_DMG_RW" "${BASE_SYSTEM_DMG_RW%%.dmg}" "$BASE_SYSTEM_DMG_RW_SPARSE"
+
+	if [ ! -z "$MNT_SPARSEIMAGE" ]; then
+		hdiutil detach -quiet -force "$MNT_SPARSEIMAGE" || rm -rf "$MNT_SPARSEIMAGE" || echo > /dev/null
+	fi
+  
+	if [[ ! -z "$TEST" && -e "$SPARSEIMAGE" ]]; then
+		rm -rf "$SPARSEIMAGE"
+	fi
 }
 
 trap cleanup EXIT INT TERM
-
 
 msg_status() {
 	echo "\033[0;32m-- $1\033[0m"
@@ -76,28 +76,34 @@ msg_status() {
 msg_error() {
 	echo "\033[0;31m-- $1\033[0m"
 }
-
-render_template() {
-	eval "echo \"$(cat "$1")\""
-}
-
-if [ $# -eq 0 ]; then
-	usage
+exit_with_error() {
+	msg_error "$1"
 	exit 1
-fi
+}
 
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 SUPPORT_DIR="$SCRIPT_DIR/support"
+
+# Import shared code to generate first boot pkgs
+FIRST_BOOT_PKG_SCRIPT="$SCRIPT_DIR/create_firstboot_pkg.sh"
+[ -f "$FIRST_BOOT_PKG_SCRIPT" ] && . "$FIRST_BOOT_PKG_SCRIPT"
 
 # Parse the optional command line switches
 USER="vagrant"
 PASSWORD="vagrant"
 IMAGE_PATH="$SUPPORT_DIR/vagrant.jpg"
+DISK_SIZE_GB=32
 
 # Flags
 DISABLE_REMOTE_MANAGEMENT=0
 DISABLE_SCREEN_SHARING=0
 DISABLE_SIP=0
+DISABLE_APFS=1
+
+if [ $# -eq 0 ]; then
+	usage
+	exit 1
+fi
 
 while getopts u:p:i:o:D: OPT; do
   case "$OPT" in
@@ -133,132 +139,126 @@ done
 shift $(expr $OPTIND - 1)
 
 if [ $(id -u) -ne 0 ]; then
-	msg_error "This script must be run as root, as it saves a disk image with ownerships enabled."
-	exit 1
+	exit_with_error "This script must be run as root, as it saves a disk image with ownerships enabled."
 fi
 
-ESD="$1"
-if [ ! -e "$ESD" ]; then
-	msg_error "Input installer image $ESD could not be found! Exiting.."
-	exit 1
+if [ -z "$(which VBoxManage)" ]; then
+	exit_with_error "VBoxManage, the command-line interface to VirtualBox, not found"
 fi
 
-if [ -d "$ESD" ]; then
-	# we might be an install .app
-	if [ -e "$ESD/Contents/SharedSupport/InstallESD.dmg" ]; then
-		ESD="$ESD/Contents/SharedSupport/InstallESD.dmg"
-	else
-		msg_error "Can't locate an InstallESD.dmg in this source location $ESD!"
-	fi
+INSTALLER_PATH="$1"
+if [ ! -e "$INSTALLER_PATH" ]; then
+	exit_with_error "Input installer image $INSTALLER_PATH could not be found! Exiting.."
 fi
 
-VEEWEE_DIR="$(cd "$SCRIPT_DIR/../../../"; pwd)"
-VEEWEE_UID=$(/usr/bin/stat -f %u "$VEEWEE_DIR")
-VEEWEE_GID=$(/usr/bin/stat -f %g "$VEEWEE_DIR")
-DEFINITION_DIR="$(cd "$SCRIPT_DIR/.."; pwd)"
-
-if [ "$2" = "" ]; then
-    msg_error "Currently an explicit output directory is required as the second argument."
-	exit 1
-	# The rest is left over from the old prepare_veewee_iso.sh script. Not sure if we
-    # should leave in this functionality to automatically locate the veewee directory.
-	DEFAULT_ISO_DIR=1
-	OLDPWD=$(pwd)
-	cd "$SCRIPT_DIR"
-	# default to the veewee/iso directory
-	if [ ! -d "../../../iso" ]; then
-		mkdir "../../../iso"
-		chown $VEEWEE_UID:$VEEWEE_GID "../../../iso"
-	fi
-	OUT_DIR="$(cd "$SCRIPT_DIR"; cd ../../../iso; pwd)"
-	cd "$OLDPWD" # Rest of script depends on being in the working directory if we were passed relative paths
-else
-	OUT_DIR="$2"
-fi
-
-if [ ! -d "$OUT_DIR" ]; then
+OUT_DIR="$2"
+if [ -z "$OUT_DIR" ]; then
+	exit_with_error "Currently an explicit output directory is required as the second argument."
+elif [ ! -d "$OUT_DIR" ]; then
 	msg_status "Destination dir $OUT_DIR doesn't exist, creating.."
 	mkdir -p "$OUT_DIR"
 fi
 
-MNT_ESD=$(/usr/bin/mktemp -d /tmp/veewee-osx-esd.XXXX)
-msg_status "Attaching input OS X installer image"
-hdiutil attach "$ESD" -mountpoint "$MNT_ESD" -nobrowse -owners on
-if [ $? -ne 0 ]; then
-	[ ! -e "$ESD" ] && msg_error "Could not find $ESD in $(pwd)"
-	msg_error "Could not mount $ESD on $MNT_ESD"
-	exit 1
+ESD="$INSTALLER_PATH/Contents/SharedSupport/InstallESD.dmg"
+if [ ! -e "$ESD" ]; then
+	exit_with_error "Can't locate an InstallESD.dmg in this source location $ESD!"
 fi
 
-msg_status "Mounting BaseSystem.."
-BASE_SYSTEM_DMG="$MNT_ESD/BaseSystem.dmg"
-MNT_BASE_SYSTEM=$(/usr/bin/mktemp -d /tmp/veewee-osx-basesystem.XXXX)
-[ ! -e "$BASE_SYSTEM_DMG" ] && msg_error "Could not find BaseSystem.dmg in $MNT_ESD"
-hdiutil attach "$BASE_SYSTEM_DMG" -mountpoint "$MNT_BASE_SYSTEM" -nobrowse -owners on
-if [ $? -ne 0 ]; then
-	msg_error "Could not mount $BASE_SYSTEM_DMG on $MNT_BASE_SYSTEM"
-	exit 1
+SYSVER_PLIST_PATH="$INSTALLER_PATH/Contents/SharedSupport/InstallInfo.plist"
+if [ ! -e "$SYSVER_PLIST_PATH" ]; then
+	exit_with_error "Can't locate InstallInfo.plist in $INSTALLER_PATH/Contents/SharedSupport/!"
 fi
-SYSVER_PLIST_PATH="$MNT_BASE_SYSTEM/System/Library/CoreServices/SystemVersion.plist"
 
-DMG_OS_VERS=$(/usr/libexec/PlistBuddy -c 'Print :ProductVersion' "$SYSVER_PLIST_PATH")
-DMG_OS_VERS_MAJOR=$(echo $DMG_OS_VERS | awk -F "." '{print $2}')
-DMG_OS_VERS_MINOR=$(echo $DMG_OS_VERS | awk -F "." '{print $3}')
-DMG_OS_BUILD=$(/usr/libexec/PlistBuddy -c 'Print :ProductBuildVersion' "$SYSVER_PLIST_PATH")
-msg_status "OS X version detected: 10.$DMG_OS_VERS_MAJOR.$DMG_OS_VERS_MINOR, build $DMG_OS_BUILD"
+DMG_OS_VERS=$(/usr/libexec/PlistBuddy -c 'Print :System\ Image\ Info:version' "$SYSVER_PLIST_PATH")
+DMG_OS_VERS_MAJOR=$(echo $DMG_OS_VERS | awk -F "." '{print $1}')
+DMG_OS_VERS_MINOR=$(echo $DMG_OS_VERS | awk -F "." '{print $2}')
+DMG_OS_VERS_PATCH=$(echo $DMG_OS_VERS | awk -F "." '{print $3}')
+msg_status "macOS version detected: $DMG_OS_VERS_MAJOR.$DMG_OS_VERS_MINOR.$DMG_OS_VERS_PATCH"
 
-msg_status "Unmounting BaseSystem.."
-hdiutil detach "$MNT_BASE_SYSTEM"
+HOST_OS_VERS=$(sw_vers -productVersion)
+HOST_OS_VERS_MAJOR=$(echo $HOST_OS_VERS | awk -F "." '{print $1}')
+HOST_OS_VERS_MINOR=$(echo $HOST_OS_VERS | awk -F "." '{print $2}')
+HOST_OS_VERS_PATCH=$(echo $HOST_OS_VERS | awk -F "." '{print $3}')
+msg_status "host macOS version detected: $HOST_OS_VERS_MAJOR.$HOST_OS_VERS_MINOR.$HOST_OS_VERS_PATCH"
+
+if [ "$DMG_OS_VERS_MAJOR" != "$DMG_OS_VERS_MAJOR" ] || [ "$DMG_OS_VERS_MINOR" != "$HOST_OS_VERS_MINOR" ]; then
+	exit_with_error "Unfortunately prepare_vdi can only generate images of same version as the host"
+fi
 
 if [ -z "$OUTPUT_DMG" ]; then
-  OUTPUT_DMG="$OUT_DIR/macOS_${DMG_OS_VERS}_${DMG_OS_BUILD}.vdi"
+  OUTPUT_DMG="$OUT_DIR/macOS_${DMG_OS_VERS}.vdi"
+elif [ -e "$OUTPUT_DMG" ]; then
+  exit_with_error "Output file $OUTPUT_DMG already exists! We're not going to overwrite it, exiting.."
 fi
 
-if [ -e "$OUTPUT_DMG" ]; then
-	msg_error "Output file $OUTPUT_DMG already exists! We're not going to overwrite it, exiting.."
-	hdiutil detach -force "$MNT_ESD"
-	exit 1
+if [ $DMG_OS_VERS_MINOR -ge 13 ]; then
+  if [ $DISABLE_APFS = 1 ]; then
+	  FSTYPE="HFS+J"
+  else
+    FSTYPE="APFS"
+  fi
+	OSPACKAGE="$INSTALLER_PATH/Contents/SharedSupport/InstallInfo.plist"
+else
+	FSTYPE="HFS+J"
+	MNT_ESD=$(/usr/bin/mktemp -d /tmp/veewee-osx-esd.XXXX)
+	
+	msg_status "Attaching input OS X installer image"
+	hdiutil attach "$ESD" -mountpoint "$MNT_ESD" -nobrowse -owners on
+	if [ $? -ne 0 ]; then
+		[ ! -e "$ESD" ] && exit_with_error "Could not find $ESD in $(pwd)"
+		exit_with_error "Could not mount $ESD on $MNT_ESD"
+	fi
+
+	OSPACKAGE="$MNT_ESD/Packages/OSInstall.mpkg"
 fi
 
 # Build our post-installation pkg that will create a user and enable ssh
 msg_status "Making firstboot installer pkg.."
 create_firstboot_pkg
 if [ -z "$BUILT_PKG" ] || [ ! -e "$BUILT_PKG" ]; then
-  msg_error "Failed building the firstboot installer pkg, exiting.."
-  exit 1
+  exit_with_error "Failed building the firstboot installer pkg, exiting.."
 fi
 
-BASE_SYSTEM_DMG_RW="$(/usr/bin/mktemp /tmp/veewee-osx-basesystem-rw.XXXX).dmg"
-DISK_SIZE_GB=32
-DISK_SIZE_BYTES=$(($DISK_SIZE_GB * 1024 * 1024 * 1024))
-msg_status "Creating empty read-write DMG located at $BASE_SYSTEM_DMG_RW.."
-hdiutil create -size "${DISK_SIZE_GB}g" -type SPARSE -fs HFS+J -volname "Macintosh HD" -uid 0 -gid 80 -mode 1775 "$BASE_SYSTEM_DMG_RW"
+MNT_SPARSEIMAGE=$(/usr/bin/mktemp -d /tmp/prepare_vdi_mnt_sparseimage.XXXX)
+SPARSEIMAGE="$(/usr/bin/mktemp /tmp/prepare_vdi.XXXX).sparseimage"
 
-BASE_SYSTEM_DMG_RW_SPARSE=$BASE_SYSTEM_DMG_RW.sparseimage
-msg_status "Mounting empty read-write DMG located at $BASE_SYSTEM_DMG_RW.."
-hdiutil attach "$BASE_SYSTEM_DMG_RW_SPARSE" -mountpoint "$MNT_BASE_SYSTEM" -nobrowse -owners on
+msg_status "Creating DMG of "${DISK_SIZE_GB}g" with $FSTYPE located at $SPARSEIMAGE.."
+if ! hdiutil create -size "${DISK_SIZE_GB}g" -type SPARSE -fs "$FSTYPE" -volname "Macintosh HD" -uid 0 -gid 80 -mode 1775 "$SPARSEIMAGE"; then
+  exit_with_error "Failed creating the disk image"
+fi
+
+msg_status "Mounting empty read-write DMG located at $SPARSEIMAGE.."
+hdiutil attach "$SPARSEIMAGE" -mountpoint "$MNT_SPARSEIMAGE" -nobrowse -owners on
 
 msg_status "Installing macOS"
-installer -verboseR -dumplog -pkg "$MNT_ESD/Packages/OSInstall.mpkg" -target "$MNT_BASE_SYSTEM"
+installer -verboseR -dumplog -pkg "$OSPACKAGE" -target "$MNT_SPARSEIMAGE"
 if [ $? -ne 0 ]; then
-	msg_error "Failed installing macOS"
-	exit 1
+	exit_with_error "Failed installing macOS"
 fi
 
 msg_status "Installing firstboot installer pkg"
-installer -pkg "$BUILT_PKG" -target "$MNT_BASE_SYSTEM"
+installer -pkg "$BUILT_PKG" -target "$MNT_SPARSEIMAGE"
 if [ $? -ne 0 ]; then
-	msg_error "Failed installing the firstboot installer pkg"
-	exit 1
+	exit_with_error "Failed installing the firstboot installer pkg"
 fi
 
 # Unmount and remount to make sure that is synchronized.
-hdiutil detach -quiet -force "$MNT_BASE_SYSTEM" || echo > /dev/null
-MOUNTOUTPUT=$(hdiutil attach "$BASE_SYSTEM_DMG_RW_SPARSE" -mountpoint "$MNT_BASE_SYSTEM" -nobrowse -owners on)
+msg_status "Remounting $SPARSEIMAGE"
+hdiutil detach -quiet -force "$MNT_SPARSEIMAGE" || echo > /dev/null
+MOUNTOUTPUT=$(hdiutil attach "$SPARSEIMAGE" -mountpoint "$MNT_SPARSEIMAGE" -nobrowse -owners on)
 DISK_DEV=$(grep GUID_partition_scheme <<< "$MOUNTOUTPUT" | cut -f1 | tr -d '[:space:]')
+DISK_SIZE_BYTES=$(($DISK_SIZE_GB * 1024 * 1024 * 1024))
 
-msg_status "Exporting $OUTPUT_DMG"
-cat "$DISK_DEV" | VBoxManage convertfromraw stdin "$OUTPUT_DMG" "$DISK_SIZE_BYTES"
+if [ ! -e "$DISK_DEV" ]; then
+	exit_with_error "Failed to find the device file of the image"
+fi
+
+msg_status "Exporting from $DISK_DEV to $OUTPUT_DMG"
+VBoxManage convertfromraw stdin "$OUTPUT_DMG" "$DISK_SIZE_BYTES" < "$DISK_DEV"
+
+msg_status "Checksumming output image.."
+MD5=$(md5 -q "$OUTPUT_DMG" | tee "$OUTPUT_DMG.md5")
+msg_status "MD5: $MD5"
 
 if [ -n "$SUDO_UID" ] && [ -n "$SUDO_GID" ]; then
 	msg_status "Fixing permissions.."
@@ -266,8 +266,7 @@ if [ -n "$SUDO_UID" ] && [ -n "$SUDO_GID" ]; then
 		"$OUT_DIR"
 fi
 
-msg_status "Checksumming output image.."
-MD5=$(md5 -q "$OUTPUT_DMG")
-msg_status "MD5: $MD5"
-
 msg_status "Done. Built image is located at $OUTPUT_DMG. Add this iso and its checksum to your template."
+
+cleanup
+exit 0
