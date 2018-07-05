99243bc0766b9b8fd5b3d656becc46712d941e50
Eric Hanko <eric.hanko1@gmail.com>
2018-07-05 16:07:53 -0700

    Prevent auto downloading High Sierra in older OSs
    - In Yosemite, the High Sierra installer is automatically downloaded
    to the applications folder when installing all updates. This change
    ignores that update.

--- scripts/system-update.sh
+++ scripts/system-update.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+
 if [ "$UPDATE_SYSTEM" != "true" ] && [ "$UPDATE_SYSTEM" != "1" ]; then
   exit
 fi
 
+MAJOR_VERSION=$(/usr/bin/sw_vers -productVersion | /usr/bin/cut -d '.' -f 2,2)
+
+if [ "$MAJOR_VERSION" -lt 13 ]; then
+  echo "Ignoring automatic High Sierra updates..."
+  softwareupdate --ignore "Install macOS High Sierra"
+fi
+
 echo "Downloading and installing system updates..."
-softwareupdate -i -a
+softwareupdate --install --all
